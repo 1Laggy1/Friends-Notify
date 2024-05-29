fceed70148b9075bfcd505b2ac8a2ab454d8641c
kropli <dimakirimail@gmail.com>
2024-05-29 23:32:10 +0300

    #3 Added Sqlite config
    - added several packages for sqlite config:
      - Microsoft.EntityFrameWrokCore.Sqlite
      - Microsoft.EntityFrameWrokCore.Tools
      - Microsoft.Extensions.Configuration
      - Microsoft.Extensions.Configuration.Json
    - created models for users representation
    - created DbContext class
    - created DbContextFactory class
    - created appsettings example file
    - added initial migration
    - updated .gitignore file
    
    closes #3

---
+++ Data/FriendsNotifyDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Friends_Notify.Data
+{
+    internal class FriendsNotifyDbContextFactory : IDesignTimeDbContextFactory<FriendsNotifyDbContext>
+    {
+        public FriendsNotifyDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var options = new DbContextOptionsBuilder<FriendsNotifyDbContext>()
+                .UseSqlite(config.GetConnectionString("MainConnectionString"))
+                .Options;
+
+            return new FriendsNotifyDbContext(options);
+
+        }
+    }
+}
