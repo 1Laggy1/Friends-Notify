081503b8bf082a21ae9e34eeecfe67756168cf5b
kropli <dimakirimail@gmail.com>
2024-05-30 16:58:24 +0300

    #1 Added users that tracking method
    - removed unused using in Program.cs
    - implemented users that tracking method
    
    closes #1

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -8,5 +8,6 @@ namespace Friends_Notify.Repositories
         public Task<List<User>> GetUsers(Expression<Func<User, bool>> predicate);
         public Task<User> AddUser(User user);
         public Task<TrackUsers> StartTrackingUser(ulong userId, ulong userToTrackId);
+        public Task<List<User>> GetUsersThatTracking(ulong userToTrackId);
     }
 }
